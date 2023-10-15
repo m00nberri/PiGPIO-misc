10ef0bccaaf9fbf926f325cb95f79fd1881d0f3f
m00nberri <zaberkat@gmail.com>
2023-10-15 03:44:35 +0100

    Add basic new view with date&time info

--- testing/urls.py
+++ testing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from testing import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("testing/<name>", views.testlink, name='testlink68')
 ]
