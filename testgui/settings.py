3440c522f1a194b207e02602c26ba9217db4b56c
m00nberri <zaberkat@gmail.com>
2023-10-15 04:48:07 +0100

    Update static path and allowed hosts

--- testgui/settings.py
+++ testgui/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)3mwmltqoj!%qdy+u%jogy5@%-mbddx^v(swb!q#1!mkx&j318
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
